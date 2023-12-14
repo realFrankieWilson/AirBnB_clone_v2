a7f2d68b8b3f1156d3d9643883ecc7bed7c037aa
realFrankieWilson <frankuwill101@gmail.com>
2023-12-14 23:51:18 +0100

    added compressed files

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A script that generates a .tgz archive from from the contents of a web.
+"""
+
+import os
+from fabric.api import local
+import datetime
+
+
+def do_pack():
+    today = datetime.datetime.now()
+    time_format = today.strftime("%Y%m%d%H%M%S")
+    web_version = f'version/web_static{time_format}.tgz'
+
+    try:
+        local("mkdir -p versions")
+        local(f'tar -cvzf {web_version} web_static')
+        return web_version
+    except Exception as e:
+        return None
