d7e7dfd0c8f728571fc86e47e3686114479f4f11
realfrankiewilson <frankuwill101@gmail.com>
2023-12-15 23:14:10 +0000

    odified file 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,64 +4,33 @@
 
 from fabric.api import *
 from datetime import datetime
-from os import path
+import os.path
 
 # Servers to log in to
 env.hosts = ['18.207.142.8', '52.86.87.208']
 env.user = 'ubuntu'
 
-
-# Function to deploy
 def do_deploy(archive_path):
-    """ Deploys a web file to server. """
-    try:
-        if not (path.exist(archive_path)):
-            return False
+	"""
+	Deploys web files to a server.
+	"""
+	if not os.path.exists(archive_path):
+		return False
 
-        # Achive upload
-        put(archive_path, '/tmp')
-
-        # Target directory
-        archive_time = archive_path[-18:-4]
-        run(
-                'sudo mkdir -p /data/web_static/releases/web_static_{}'.format(
-                    archive_time)
-                )
-
-        # Uncompress archive and delete .tgz files.
-        run(
-                'sudo tar -xzf /tmp/web_static_{}.tgz -c\
-                /data/web_static_releases/web_static_{}/'.format(
-                    archive_time, archive_time)
-                )
-
-        # Removes archive files.
-        run('sudo rm /tmp/web_static_{}.tgz'.format(archive_time))
-
-        # Move the contents into the host web_static
-        run(
-                'sudo mv /data/web_static/releases/web_static_{}/web_static/*\
-                /data/web_static/releases/web_static_{}/'.format(
-                    archive_time, archive_time)
-                )
-
-        # Remove extraneous web_static directory
-        run(
-                'sudo rm -rf /data/web_static/releases/\
-                web_static_{}/web_static'.format(archive_time)
-                )
-
-        # Delete pre-existing symbolic link
-        run('sudo rm -rf /data/web_static/current')
-
-        # Re-establish symbolic link
-        run(
-                'sudo ln -s /data/web_static/releases/\
-                web_static_{}/ /data/web_static/current'.format(archive_time)
-                )
-
-    except Exception as e:
-        return False
-
-    # Otherwise True is return
-    return True
+	file_name = archive_path.split('/')
+	new_name = file_name[1]
+	n_var = new_name.split('.')
+	path_tar = "/data/web_static/releases/{}/".format(n_var[0])
+	symbolic_link = "/data/web_static/current"
+	
+	try:
+	    put(archive_path, "/tmp/")
+	    run("mkdir -p {}".format(path_tar))
+	    run("tar -xzf /tmp/{} -C {}".format(new_name, path_tar))
+	    run("rm /tmp/{}".format(new_name))
+	    run("mv {}web_static/* {}".format(path_tar, path_tar))
+	    run("rm -rf {}".format(symbolic_link))
+	    run("ln -s {} {}".format(path_tar, symbolic_link))
+	    return True
+	except:
+		return False
