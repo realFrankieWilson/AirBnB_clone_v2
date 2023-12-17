6cfdfb4bd7a168f1e5a04865924bb50ffc4fc36c
realfrankiewilson <frankuwill101@gmail.com>
2023-12-17 10:48:29 -0800

    advanced task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,6 +3,7 @@
 script based on the file 1-pack_web_static.py
 """
 from fabric.api import *
+import os
 
 # Servers to log in to
 env.hosts = ['18.207.142.8', '52.86.87.208']
@@ -11,14 +12,14 @@ env.user = 'ubuntu'
 
 def do_clean(number=0):
     """ Dlelets contents."""
-    number = int(number)
+    number = 1 if int(number) == 0 else int(number)
 
-    if number == 0 or number == 1:
-        local('cd versions; ls | head -n -1 | xargs rm -fr')
-        run('cd /data/web_static/releases ; ls | head -n -1 | xargs rm -rf')
-    else:
-        local('cd versions; ls | head -n -{} | xargs rm -rf'.format(number))
-        run(
-                'cd /data/web_static/releases ; ls | head -n -2 | xargs rm -rf\
-                        '.format(number)
-                )
+    arch = sorted(os.listdir("versions"))
+    [arch.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in arch]
+
+    with cd("/data/web_static/releases"):
+        arch = run("ls -tr").split()
+        arch = [a for a in arch if "web_static_" in a]
+        [run("rm -rf ./{}".format(a)) for a in arch]
