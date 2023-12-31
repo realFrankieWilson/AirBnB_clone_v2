568fc0e02dc302089834cf82342703d388eeb140
realFrankieWilson <frankuwill101@gmail.com>
2023-12-31 17:04:14 +0100

    added readme file and task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A module that contains functions that starts application
+"""
+from flask import Flask
+
+
+# Create an instance of Flask class.
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    '''
+    Starts a Flask web application:
+        Web listens 0.0.0.0, port 5000
+        Routs: -> display "Hello HBNB!"
+    '''
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
