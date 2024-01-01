9bee9a7bf32acb6859f6ed8f4e66786f1dcb1d11
realfrankiewilson <frankuwill101@gmail.com>
2024-01-01 10:33:51 -0800

    hot fix file 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@ A module that contains functions that starts application
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 # Create an instance of Flask class.
@@ -27,6 +28,7 @@ def display_state(n):
         Web listens 0.0.0.0, port 5000
         /number: -> Displays states.
     '''
+    states = storage.all(State).values()
     return render_template('7-states_list.html', state=states)
 
 
