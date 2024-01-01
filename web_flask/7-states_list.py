77104bde10441c804d2fc213a096e51c0f9dc16c
realfrankiewilson <frankuwill101@gmail.com>
2023-12-31 17:29:53 -0800

    added task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A module that contains functions that starts application
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+# Create an instance of Flask class.
+app = Flask(__name__)
+# Allows route to match requests with or without trailing whitespace.
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close_sess(error):
+    '''
+    Closes the session
+    '''
+    storage.close()
+
+
+@app.route("/states_list")
+def display_state(n):
+    '''
+    Starts a Flask web application:
+        Web listens 0.0.0.0, port 5000
+        /number: -> Displays states.
+    '''
+    return render_template('7-states_list.html', state=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
